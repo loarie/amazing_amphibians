70baec2b80afd70acebd25854eea72a6b1316936
Scott Loarie <loarie@gmail.com>
2013-03-10 15:53:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AmazingAmphibians::Application.config.secret_token = '4c85c43650e74dfe82300362ca29a5b11b172fb187086a0e1c5e27202fe2d1cdb7aa2a495a53ae9e9840372b90f5c0fba581f26264468ae8a4b4989aeefdfe32'
