446117e08a84f8ea2bff0d55cfc0e781df99228d
Scott Loarie <loarie@gmail.com>
2013-03-10 17:58:34 -0700

    minor changes to banner

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,7 +1,12 @@
 class MapsController < ApplicationController
-
+  require 'open-uri'
+  require 'json'
+  
   def home
     @taxon_id = params[:taxon_id]
+    url = "http://www.inaturalist.org/taxa/#{@taxon_id}.json"
+    result = JSON.parse(open(url).read)
+    @name = result["name"]
   end
 
 end
